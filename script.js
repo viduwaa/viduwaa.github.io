e15690631286b56df62e04d2cba983e3e7645910
Vidula Deneth Salwathura <105682219+viduwaa@users.noreply.github.com>
2023-08-26 01:23:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+window.onload = function () {
+  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  let interval = null;
+
+  document.querySelector("h1").onmouseover = (event) => {
+    let iteration = 0;
+
+    clearInterval(interval);
+
+    interval = setInterval(() => {
+      event.target.innerText = event.target.innerText
+        .split("")
+        .map((letter, index) => {
+          if (index < iteration) {
+            return event.target.dataset.value[index];
+          }
+
+          return letters[Math.floor(Math.random() * 26)];
+        })
+        .join("");
+
+      if (iteration >= event.target.dataset.value.length) {
+        clearInterval(interval);
+      }
+
+      iteration += 1 / 3;
+    }, 30);
+  };
+};
+
+
+
+
 const primaryNav = document.querySelector(".nav-menu");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 

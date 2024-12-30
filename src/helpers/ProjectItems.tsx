c8d89fc8dcd67b3fe0d88c34c9588bf5d99e5ee7
Vidula Deneth <viduladenethsalwathura@gmail.com>
2024-12-31 00:23:00 +0530

    added new project

--- src/helpers/ProjectItems.tsx
+++ src/helpers/ProjectItems.tsx
@@ -1,82 +1,87 @@
-import PrjHooks from "/assets/projects/pr_hooks.webp"
-import PrjReact from "/assets/projects/pr_react.webp"
-import PrjHospital from "/assets/projects/pr_hospital.webp"
-import PrjMal from "/assets/projects/pr_mal.webp"
-import PrjTower from "/assets/projects/pr_tower.webp"
-import PrjYT2 from "/assets/projects/pr_yt2spt.webp"
-import PrjStore from "/assets/projects/pr_store.webp"
-import PrjMalkoha from "/assets/projects/pr_malkoha.webp"
-import PrjGaming from "/assets/projects/pr_gaming.webp"
-
-
-
-
+import PrjHooks from "/assets/projects/pr_hooks.webp";
+import PrjReact from "/assets/projects/pr_react.webp";
+import PrjHospital from "/assets/projects/pr_hospital.webp";
+import PrjMal from "/assets/projects/pr_mal.webp";
+import PrjTower from "/assets/projects/pr_tower.webp";
+import PrjYT2 from "/assets/projects/pr_yt2spt.webp";
+import PrjStore from "/assets/projects/pr_store.webp";
+import PrjMalkoha from "/assets/projects/pr_malkoha.webp";
+import PrjGaming from "/assets/projects/pr_gaming.webp";
 
 export const ProjectItems = [
     {
-        name:"Firebase Mini-Gaming Site",
-        background:PrjGaming,
-        description:"A mini gaming website with React and Firebase backend",
-        webSite:"https://firebase-mini-gaming-site.pages.dev/",
-        gitRepo:"https://github.com/viduwaa/firebase-gaming-site"
+        name: "Library Management System",
+        background: PrjGaming,
+        description: "1st Year University Project",
+        webSite: "https://library.malkoha.site/",
+        gitRepo: "https://github.com/viduwaa/library-management-system",
     },
     {
-        name:"Student Materials Website",
-        background:PrjMalkoha,
-        description:"A simple UI based website for university students to find study materials",
-        webSite:"https://malkoha.site",
-        gitRepo:"https://github.com/viduwaa/malkohav2"
+        name: "Firebase Mini-Gaming Site",
+        background: PrjGaming,
+        description: "A mini gaming website with React and Firebase backend",
+        webSite: "https://firebase-mini-gaming-site.pages.dev/",
+        gitRepo: "https://github.com/viduwaa/firebase-gaming-site",
     },
     {
-        name:"Quiz App",
-        background:PrjHooks,
-        description:"A quiz App intergrated with OpenTDB API",
-        webSite:"https://viduwaa.github.io/hooks_learn/",
-        gitRepo:"https://github.com/viduwaa/hooks_learn/"
+        name: "Student Materials Website",
+        background: PrjMalkoha,
+        description:
+            "A simple UI based website for university students to find study materials",
+        webSite: "https://malkoha.site",
+        gitRepo: "https://github.com/viduwaa/malkohav2",
     },
     {
-        name:"Dynamic Shopping Cart Demo",
-        background:PrjStore,
-        description:"Simple shopping cart to understand React useContext and React router",
-        webSite:"https://viduwaa.github.io/react-store-example/",
-        gitRepo:"https://github.com/viduwaa/react-store-example"
+        name: "Quiz App",
+        background: PrjHooks,
+        description: "A quiz App intergrated with OpenTDB API",
+        webSite: "https://viduwaa.github.io/hooks_learn/",
+        gitRepo: "https://github.com/viduwaa/hooks_learn/",
     },
     {
-        name:"1st React App",
-        background:PrjReact,
-        description:"Built for understanding how REACT works w/ TS",
-        webSite:"https://viduwaa.github.io/reactbasics-with-demo-website/",
-        gitRepo:"https://github.com/viduwaa/reactbasics-with-demo-website"
+        name: "Dynamic Shopping Cart Demo",
+        background: PrjStore,
+        description:
+            "Simple shopping cart to understand React useContext and React router",
+        webSite: "https://viduwaa.github.io/react-store-example/",
+        gitRepo: "https://github.com/viduwaa/react-store-example",
     },
     {
-        name:"Health Care website UI",
-        background:PrjHospital,
-        description:"Created as assignment for academics.",
-        webSite:"https://viduwaa.github.io/healthcare-website-demo",
-        gitRepo:"https://github.com/viduwaa/healthcare-website-demo"
+        name: "1st React App",
+        background: PrjReact,
+        description: "Built for understanding how REACT works w/ TS",
+        webSite: "https://viduwaa.github.io/reactbasics-with-demo-website/",
+        gitRepo: "https://github.com/viduwaa/reactbasics-with-demo-website",
     },
     {
-        name:"University Pet Project",
-        background:PrjMal,
-        description:"Simple UI design for university students for find the study materials",
-        webSite:"https://malkhoa-test.onrender.com/#",
-        gitRepo:"https://github.com/viduwaa/malkoha-test"
+        name: "Health Care website UI",
+        background: PrjHospital,
+        description: "Created as assignment for academics.",
+        webSite: "https://viduwaa.github.io/healthcare-website-demo",
+        gitRepo: "https://github.com/viduwaa/healthcare-website-demo",
     },
     {
-        name:"Tower Defense Godot",
-        background:PrjTower,
-        description:"A simple game made from Godot to understand basics of game dev",
-        webSite:null,
-        gitRepo:"https://github.com/viduwaa/towerdefense-godot"
+        name: "University Pet Project",
+        background: PrjMal,
+        description:
+            "Simple UI design for university students for find the study materials",
+        webSite: "https://malkhoa-test.onrender.com/#",
+        gitRepo: "https://github.com/viduwaa/malkoha-test",
     },
     {
-        name:"Youtube to Spotify",
-        background:PrjYT2,
-        description:"Simple program for importing youtube playlist to spotify.",
-        webSite:null,
-        gitRepo:"https://github.com/viduwaa/yt-playlist-to-spotify"
+        name: "Tower Defense Godot",
+        background: PrjTower,
+        description:
+            "A simple game made from Godot to understand basics of game dev",
+        webSite: null,
+        gitRepo: "https://github.com/viduwaa/towerdefense-godot",
     },
-    
-    
-    
-]
+    {
+        name: "Youtube to Spotify",
+        background: PrjYT2,
+        description:
+            "Simple program for importing youtube playlist to spotify.",
+        webSite: null,
+        gitRepo: "https://github.com/viduwaa/yt-playlist-to-spotify",
+    },
+];
